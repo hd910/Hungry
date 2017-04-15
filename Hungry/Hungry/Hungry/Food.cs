70446587d3213aadfce5726ab3e2b37c24d3f405
hd910 <hd910@outlook.com>
2017-04-15 12:15:35 +1200

    Dynamically populate preview food images

---
+++ Hungry/Hungry/Hungry/Food.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hungry
+{
+    class Food
+    {
+        public string description { get; set; }
+        public string uri { get; set; }
+
+        public Food(string description, string uri)
+        {
+            this.description = description;
+            this.uri = uri;
+        }
+
+        
+    }
+}
--- Hungry/Hungry/Hungry/MainPage.xaml.cs
+++ Hungry/Hungry/Hungry/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ImageCircle.Forms.Plugin.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,45 @@ namespace Hungry
 {
     public partial class MainPage : ContentPage
     {
+        private Food[] foodList = new Food[10];
         public MainPage()
         {
             InitializeComponent();
+            constructFoodArray();
+            loadPreviewIcons();
+        }
+
+        private void constructFoodArray()
+        {
+            for(var i = 0; i < 4; i++)
+            {
+                foodList[i] = new Food(string.Format("This is food {0}", i), 
+                                string.Format("http://lorempixel.com/output/food-q-c-80-80-{0}.jpg", (i+1)));
+            }
+        }
+
+        private void loadPreviewIcons()
+        {
+            
+            for(var i = 0; i < foodList.Length; i++)
+            {
+                Food food = foodList[i];
+                if(food != null)
+                {
+                    string uri = foodList[i].uri;
+                    var tempIcon = new CircleImage
+                    {
+                        HeightRequest = 50,
+                        WidthRequest = 50,
+                        Aspect = Aspect.AspectFill,
+                        Source = UriImageSource.FromUri(new Uri(uri))
+                    };
+
+                    //tempIcon.GestureRecognizers.Add(new TapGestureRecognizer(OnTap));
+                    previewImagesLayout.Children.Add(tempIcon);
+                }
+                
+            }
         }
     }
 }
