88b5a20b64c28c2441f3d658a2f30c3505a7be6e
hd910 <hd910@outlook.com>
2017-04-16 20:19:30 +1200

    Fetch images from Flickr

--- Hungry/Hungry/Hungry/MainPage.xaml.cs
+++ Hungry/Hungry/Hungry/MainPage.xaml.cs
@@ -1,40 +1,53 @@
 ﻿using ImageCircle.Forms.Plugin.Abstractions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
+using System.Xml.Linq;
 using Xamarin.Forms;
 
 namespace Hungry
 {
     public partial class MainPage : ContentPage
     {
-        private Food[] foodList = new Food[10];
-        private string url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={0}&tags={1}&safe_search=1&per_page={31}";
-
+        private List<Food> foodList = new List<Food>();
+        private string url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={0}&text={1}&safe_search=1&per_page={2}&sort=relevance";
+        private HttpClient _client = new HttpClient();
 
         public MainPage()
         {
             InitializeComponent();
-            constructFoodArray();
 
+            loadImages();
         }
 
-        private void constructFoodArray()
+        private async void loadImages()
         {
-            for(var i = 0; i < 4; i++)
+
+            var formattedurl = string.Format(url, APIKeys.FLICKR_API_KEY, "Pizza", "5");
+
+            var content = await _client.GetStringAsync(formattedurl);
+
+            var xdoc = XDocument.Parse(content);
+            foreach (var node in xdoc.Descendants("photos").Descendants("photo"))
             {
-                foodList[i] = new Food(string.Format("This is food {0}", i), 
-                                string.Format("http://lorempixel.com/output/food-q-c-80-80-{0}.jpg", (i+1)));
+                var id = node.Attribute("id").Value;
+                var secretId = node.Attribute("secret").Value;
+                var farmId = node.Attribute("farm").Value;
+                var serverId = node.Attribute("server").Value;
+
+                var imageURL = string.Format("https://farm{0}.staticflickr.com/{1}/{2}_{3}_c.jpg", farmId, serverId, id, secretId);
+
+                foodList.Add(new Food("This is a pizza", imageURL));
             }
+
             loadPreviewIcons();
+
         }
 
         private void loadPreviewIcons()
         {
             
-            for(var i = 0; i < foodList.Length; i++)
+            for(var i = 0; i < foodList.Count; i++)
             {
                 Food food = foodList[i];
                 if(food != null)
