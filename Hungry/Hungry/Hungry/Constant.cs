2734b3a4610dbdd79d3479d98bd2e7fced27458d
hd910 <hd910@outlook.com>
2017-04-16 16:48:06 +1200

    Add keys

---
+++ Hungry/Hungry/Hungry/Constant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hungry
+{
+    class Constant
+    {
+        public static string FLICKR_API_KEY { get { return "ADD API HERE"; } }
+    }
+}
