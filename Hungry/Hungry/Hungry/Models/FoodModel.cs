f1cadb13acf9e4bbaba5eb70e0635965cd71dbb2
hd910 <hd910@outlook.com>
2017-06-25 11:04:10 +1200

    Add azure mobile services nuget + food model + azure manager

---
+++ Hungry/Hungry/Hungry/Models/FoodModel.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hungry.Models
+{
+    class FoodModel
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string ID { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "url1")]
+        public string URL1 { get; set; }
+
+        [JsonProperty(PropertyName = "url1thumb")]
+        public string URL1Thumb { get; set; }
+    }
+}
